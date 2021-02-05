b101aced05bdd3a261f7e253eadbe38115fe9aec
Windows 10 <emirbajnazarov44@gmail.com>
2021-02-05 20:10:23 +0600

    rename text view, add image

--- app/src/main/java/com/example/lessongit/MainActivity.java
+++ app/src/main/java/com/example/lessongit/MainActivity.java
@@ -6,12 +6,12 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView helloTv;
+    TextView MyTextVirw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        helloTv = findViewById(R.id.hello_text_view);
+        MyTextVirw = findViewById(R.id.hello_text_view);
     }
 }
