db56a4027852e7264dffe9e1591d9e3996f4b316
Windows 10 <emirbajnazarov44@gmail.com>
2021-02-05 20:05:02 +0600

    created text view

--- app/src/main/java/com/example/lessongit/MainActivity.java
+++ app/src/main/java/com/example/lessongit/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.lessongit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView helloTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        helloTv = findViewById(R.id.hello_text_view);
     }
 }
