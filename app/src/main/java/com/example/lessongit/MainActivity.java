4dba9b9905dbe21c6414cc5b79374a70e443d579
Windows 10 <emirbajnazarov44@gmail.com>
2021-02-08 19:00:50 +0600

    add SecondActivity

--- app/src/main/java/com/example/lessongit/MainActivity.java
+++ app/src/main/java/com/example/lessongit/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.lessongit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myButton = findViewById(R.id.second_button);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
