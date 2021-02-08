4d7027b2933ef6ad0cadf30e8a36e82efbfe6c8a
Windows 10 <emirbajnazarov44@gmail.com>
2021-02-08 18:33:01 +0600

    add button

--- app/src/main/java/com/example/lessongit/MainActivity.java
+++ app/src/main/java/com/example/lessongit/MainActivity.java
@@ -3,15 +3,16 @@ package com.example.lessongit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView MyTextVirw;
+    Button myButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MyTextVirw = findViewById(R.id.hello_text_view);
+        myButton = findViewById(R.id.second_button);
     }
 }
